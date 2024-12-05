1984a5aa3beb480487db05168e1ddde5d92918cf
Gregory Denys <gregory.g.denys@gmail.com>
2024-12-04 22:49:00 -0500

    first nix-darwin flake addition

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dotfiles
 My personal home configurations
+
+## Usage
+
+1. To make updates to the system after modifying a flake:
+   
+```bash
+% cd ./nix-darwin 
+% darwin-rebuild switch --flake .
+```
+
+2. If needing to debug the flake, add NIX_DEBUG=1. Note that running the following command without the `switch` statement will terminate the execution.  This is useful for debugging purposes.
+
+```bash
+% cd ./nix-darwin 
+% NIX_DEBUG=1 darwin-rebuild switch --flake .
+```
---
+++ nix-darwin/flake.nix
@@ -0,0 +1,65 @@
+{
+	description = "My nix-darwin system flake";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nix-darwin.url = "github:LnL7/nix-darwin";
+		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	outputs = inputs@{ self, nix-darwin, nixpkgs }:
+	let
+		configuration = { pkgs, ... }: {
+		# List packages installed in system profile. To search by name, run:
+		# $ nix-env -qaP | grep wget
+		environment.systemPackages =
+			[ 
+				pkgs.vim
+			];
+
+		services.nix-daemon.enable = true;
+		nix.settings.experimental-features = "nix-command flakes";
+		# default shell on catalina
+		programs.zsh.enable = true;  
+		# Set Git commit hash for darwin-version.
+		system.configurationRevision = self.rev or self.dirtyRev or null; 
+		system.stateVersion = 5;
+		# The platform the configuration will be used on.
+		nixpkgs.hostPlatform = "aarch64-darwin";
+		security.pam.enableSudoTouchIdAuth = true;
+
+		# Enable alternative shell support in nix-darwin.
+		# programs.fish.enable = true;
+		
+		# Used for backwards compatibility, please read the changelog before changing.
+		# $ darwin-rebuild changelog
+
+		users.users.gdenys.home = "/Users/gdenys";
+		# home-manager.backupFileExtension = "backup";
+		nix.configureBuildUsers = true;
+		nix.useDaemon = true;
+
+		system.defaults = {
+			dock.autohide = true;
+			dock.mru-spaces = false;
+			finder.AppleShowAllExtensions = false;
+			finder.AppleShowAllFiles = true;
+			finder.FXPreferredViewStyle = "clmv";
+			loginwindow.LoginwindowText = "Welcome!";
+			screencapture.location = "~/Desktop/Screenshots2";
+			screensaver.askForPasswordDelay = 10;
+		};
+    };
+  	in
+	{
+		# Build darwin flake using:
+		# $ darwin-rebuild build --flake .#Gregorys-MacBook-Pro
+		darwinConfigurations."Gregorys-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+			system = "aarch64-darwin";	
+			modules = [ configuration ];
+		};
+
+		# Expose the package set, including overlays, for convenience.
+    	darwinPackages = self.darwinConfigurations."Gregorys-MacBook-Pro".pkgs;
+	};
+}
