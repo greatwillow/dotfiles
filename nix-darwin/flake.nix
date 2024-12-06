1e8f9a5fee45662dd9ce9222dcb8bcf7219c52f8
Gregory Denys <gregory.g.denys@gmail.com>
2024-12-05 21:10:51 -0500

    add initial home manager config

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,9 +5,13 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 		nix-darwin.url = "github:LnL7/nix-darwin";
 		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+		home-manager = {
+      		url = "github:nix-community/home-manager";
+      		inputs.nixpkgs.follows = "nixpkgs";
+    	};
 	};
 
-	outputs = inputs@{ self, nix-darwin, nixpkgs }:
+	outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
 	let
 		configuration = { pkgs, ... }: {
 		# List packages installed in system profile. To search by name, run:
@@ -56,7 +60,15 @@
 		# $ darwin-rebuild build --flake .#Gregorys-MacBook-Pro
 		darwinConfigurations."Gregorys-MacBook-Pro" = nix-darwin.lib.darwinSystem {
 			system = "aarch64-darwin";	
-			modules = [ configuration ];
+			modules = [ 
+				configuration
+				home-manager.darwinModules.home-manager 
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.gdenys = import ./home.nix;
+				}
+			];
 		};
 
 		# Expose the package set, including overlays, for convenience.
