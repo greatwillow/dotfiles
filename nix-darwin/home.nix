22d7a0394eb1272e36ebc778965216fd319ac03d
Gregory Denys <gregory.g.denys@gmail.com>
2024-12-08 16:51:39 -0500

    add nushell with base config

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -11,24 +11,25 @@
 	# Makes sense for user specific applications that shouldn't be available system-wide
 	home.packages = [
 		pkgs.wezterm
+		pkgs.nushell
 	];
 
 	# Home Manager is pretty good at managing dotfiles. The primary way to manage
 	# plain files is through 'home.file'.
 	home.file = {
-		# ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
+		# ".zshrc".source = /Users/gdenys/dotfiles/zshrc/.zshrc;
 		".config/wezterm".source = /Users/gdenys/dotfiles/wezterm;
-		# ".config/skhd".source = ~/dotfiles/skhd;
-		# ".config/starship".source = ~/dotfiles/starship;
-		# ".config/zellij".source = ~/dotfiles/zellij;
-		# ".config/nvim".source = ~/dotfiles/nvim;
+		# ".config/skhd".source = /Users/gdenys/dotfiles/skhd;
+		# ".config/starship".source = /Users/gdenys/dotfiles/starship;
+		# ".config/zellij".source = /Users/gdenys/dotfiles/zellij;
+		# ".config/nvim".source = /Users/gdenys/dotfiles/nvim;
 		".config/nix".source = /Users/gdenys/dotfiles/nix;
 		".config/nix-darwin".source = /Users/gdenys/dotfiles/nix-darwin;
-		# ".config/tmux".source = ~/dotfiles/tmux;
-		# ".config/ghostty".source = ~/dotfiles/ghostty;
-		# ".config/aerospace".source = ~/dotfiles/aerospace;
-		# ".config/sketchybar".source = ~/dotfiles/sketchybar;
-		# ".config/nushell".source = ~/dotfiles/nushell;
+		# ".config/tmux".source = /Users/gdenys/dotfiles/tmux;
+		# ".config/ghostty".source = /Users/gdenys/dotfiles/ghostty;
+		# ".config/aerospace".source = /Users/gdenys/dotfiles/aerospace;
+		# ".config/sketchybar".source = /Users/gdenys/dotfiles/sketchybar;
+		".config/nushell".source = /Users/gdenys/dotfiles/nushell;
 	};
 
 	home.sessionVariables = {
