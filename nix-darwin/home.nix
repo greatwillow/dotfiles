31d560fa4ec5f5d347b67299a2ebc04cce7308a7
Gregory Denys <gregory.g.denys@gmail.com>
2024-12-08 20:17:50 -0500

    add _common config folder to nix

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -17,19 +17,20 @@
 	# Home Manager is pretty good at managing dotfiles. The primary way to manage
 	# plain files is through 'home.file'.
 	home.file = {
+		".config/_common".source = "/Users/gdenys/dotfiles/_common";
 		# ".zshrc".source = /Users/gdenys/dotfiles/zshrc/.zshrc;
-		".config/wezterm".source = /Users/gdenys/dotfiles/wezterm;
+		".config/wezterm".source = "/Users/gdenys/dotfiles/wezterm";
 		# ".config/skhd".source = /Users/gdenys/dotfiles/skhd;
 		# ".config/starship".source = /Users/gdenys/dotfiles/starship;
 		# ".config/zellij".source = /Users/gdenys/dotfiles/zellij;
 		# ".config/nvim".source = /Users/gdenys/dotfiles/nvim;
-		".config/nix".source = /Users/gdenys/dotfiles/nix;
-		".config/nix-darwin".source = /Users/gdenys/dotfiles/nix-darwin;
+		".config/nix".source = "/Users/gdenys/dotfiles/nix";
+		".config/nix-darwin".source = "/Users/gdenys/dotfiles/nix-darwin";
 		# ".config/tmux".source = /Users/gdenys/dotfiles/tmux;
 		# ".config/ghostty".source = /Users/gdenys/dotfiles/ghostty;
 		# ".config/aerospace".source = /Users/gdenys/dotfiles/aerospace;
 		# ".config/sketchybar".source = /Users/gdenys/dotfiles/sketchybar;
-		".config/nushell".source = /Users/gdenys/dotfiles/nushell;
+		".config/nushell".source = "/Users/gdenys/dotfiles/nushell";
 	};
 
 	home.sessionVariables = {
