6cf6cd3c419135fadd199b0c8b3d060b38519f6c
Gregory Denys <gregory.g.denys@gmail.com>
2025-01-11 17:38:21 -0500

    update zsh profile to remove plugins

--- homeManagerModules/_common/session_variables.nix
+++ homeManagerModules/_common/session_variables.nix
@@ -1,4 +1,3 @@
-
 {
   pkgs,
   rootConfigPath,
@@ -6,34 +5,36 @@
   isLinuxOS,
   isWindowsOS,
   ...
-}: let
-    sessionVariables = {
-		IS_MAC_OS = if isMacOS then "true" else "false";
-		IS_LINUX_OS = if isLinuxOS then "true" else "false";
-		IS_WINDOWS_OS = if isWindowsOS then "true" else "false";
-		# Editor
-		EDITOR = "nvim";
-		VISUAL = "nvim";
-		# XDG config home
-		XDG_CONFIG_HOME = "${rootConfigPath}";
-		# FZF
-		# Here we are using `rg` (ripgrep) as the default command for `fzf`
-		# If we comment out this line and run `fzf` in the terminal, it will use `find` as the default command
-		FZF_DEFAULT_COMMAND = "rg --files --hidden --glob '!.git'";
-		FZF_DEFAULT_OPTS = "--height=40% --layout=reverse --border --margin=1 --padding=1";
-		# BAT
-		BAT_THEME = "gruvbox-dark";
-		# DIRCOLORS (MacOS)
-		CLICOLOR = "1";
-		# Add coloring to man pages text
-		LESS_TERMCAP_mb = "\e[1;32m";
-		LESS_TERMCAP_md = "\e[1;32m";
-		LESS_TERMCAP_me = "\e[0m";
-		LESS_TERMCAP_se = "\e[0m";
-		LESS_TERMCAP_so = "\e[01;33m";
-		LESS_TERMCAP_ue = "\e[0m";
-		LESS_TERMCAP_us = "\e[1;4;31m";
-    };
-in {
-    sessionVariables = sessionVariables;
+}:
+let
+  sessionVariables = {
+    IS_MAC_OS = if isMacOS then "true" else "false";
+    IS_LINUX_OS = if isLinuxOS then "true" else "false";
+    IS_WINDOWS_OS = if isWindowsOS then "true" else "false";
+    # Editor
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    # XDG config home
+    XDG_CONFIG_HOME = "${rootConfigPath}";
+    # FZF
+    # Here we are using `rg` (ripgrep) as the default command for `fzf`
+    # If we comment out this line and run `fzf` in the terminal, it will use `find` as the default command
+    FZF_DEFAULT_COMMAND = "rg --files --hidden --glob '!.git'";
+    FZF_DEFAULT_OPTS = "--height=40% --layout=reverse --border --margin=1 --padding=1";
+    # BAT
+    BAT_THEME = "gruvbox-dark";
+    # DIRCOLORS (MacOS)
+    CLICOLOR = "1";
+    # Add coloring to man pages text
+    LESS_TERMCAP_mb = "\e[1;32m";
+    LESS_TERMCAP_md = "\e[1;32m";
+    LESS_TERMCAP_me = "\e[0m";
+    LESS_TERMCAP_se = "\e[0m";
+    LESS_TERMCAP_so = "\e[01;33m";
+    LESS_TERMCAP_ue = "\e[0m";
+    LESS_TERMCAP_us = "\e[1;4;31m";
+  };
+in
+{
+  sessionVariables = sessionVariables;
 }
--- homeManagerModules/zsh/zsh.nix
+++ homeManagerModules/zsh/zsh.nix
@@ -1,33 +1,31 @@
 {
-  	pkgs,
-  	shellAliases,
-		homeManagerModulesPath,
-  	...
-}: 
+  pkgs,
+  shellAliases,
+  homeManagerModulesPath,
+  ...
+}:
 {
-		zsh = {
-				enable = true;
-				enableCompletion = true;
-				autosuggestion.enable = true;
-				syntaxHighlighting.enable = true;
-				syntaxHighlighting.highlighters = [
-						"main"
-						"brackets"
-				];
-				shellAliases = shellAliases;	
-				history = {
-						size = 100000;
-						# substringSearch.enable = true;
-				};
-				antidote.enable = true;
-				antidote.plugins = [
-						"zsh-users/zsh-autosuggestions"
-						"jeffreytse/zsh-vi-mode"
-				];	
-				initExtra = builtins.replaceStrings ["\r\n"] ["\n"] ''
-						if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
-								eval "$(oh-my-posh init zsh --config ${homeManagerModulesPath}/oh_my_posh/themes/catppuccin_mocha.omp.json)"
-						fi
-				'';
-		};
+  zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    syntaxHighlighting.highlighters = [
+      "main"
+      "brackets"
+    ];
+    shellAliases = shellAliases;
+    history = {
+      size = 100000;
+      # substringSearch.enable = true;
+    };
+    antidote.enable = true;
+    antidote.plugins = [
+    ];
+    initExtra = builtins.replaceStrings [ "\r\n" ] [ "\n" ] ''
+      
+            						if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+            								eval "$(oh-my-posh init zsh --config ${homeManagerModulesPath}/oh_my_posh/themes/catppuccin_mocha.omp.json)"
+            						fi
+            				'';
+  };
 }
